e51f4f4cd15580d3ae8c42f26816688daeddf338
specter <2928781682@qq.com>
2026-03-16 21:46:17 +0800

    add helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,17 @@
+/**
+ * HelloWorld 示例程序
+ * * @author YourName
+ * @version 1.0.0
+ * @description 遵循 Google Java 编程风格指南的基础入门示例
+ */
+public class HelloWorld {
+
+    /**
+     * 程序主入口点
+     * * @param args 命令行传入的参数数组
+     */
+    public static void main(String[] args) {
+        // 向标准输出（STDOUT）打印字符串
+        System.out.println("Hello, World! 来自西南石油大学的后端开发者。");
+    }
+}
